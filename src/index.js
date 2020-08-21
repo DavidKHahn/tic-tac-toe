5c996216468170284ff96e6715336c448d255196
David <dhahn123@gmail.com>
2020-08-21 14:08:36 -0700

    fix typo

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function Square(props) {
       return (
         <Square
           value={this.props.squares[i]}
-          onClick={() => this.props.handleClick(i)}
+          onClick={() => this.props.onClick(i)}
         />
       )
     }
